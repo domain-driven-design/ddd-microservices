9e8e92ea41d6f534c8a68c56a91665d64bfc1460
zyao6449 <zyao6449@163.com>
2024-02-06 17:13:27 +0800

    feat: init calculation

---
+++ calculation/src/main/java/com/example/calculation/CalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculationApplication.class, args);
+    }
+
+}
---
+++ calculation/src/test/java/com/example/calculation/CalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
