a07f19dfd0a3f56ecf8be27b172a500353ebc6cc
Ashley <wanglu.ashelin@gmail.com>
2024-02-27 18:10:28 +0800

    feat: controller

---
+++ base/src/main/java/com/ddd/base/adapter/UserController.java
@@ -0,0 +1,14 @@
+package com.ddd.base.adapter;
+
+import com.ddd.base.application.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+}
