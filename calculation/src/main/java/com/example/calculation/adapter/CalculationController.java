dc704472d3f776db1a26d7eb272be43e2bc37e89
zyao6449 <zyao6449@163.com>
2024-03-04 21:39:21 +0800

    feat: swagger

--- calculation/src/main/java/com/example/calculation/adapter/CalculationController.java
+++ calculation/src/main/java/com/example/calculation/adapter/CalculationController.java
@@ -5,6 +5,8 @@ import com.example.calculation.application.args.CalculationCommand;
 import com.example.calculation.application.dto.CalculationNodeResult;
 import com.example.calculation.application.service.CalculationAppService;
 import com.example.calculation.domain.aggregate.AbstractCalculationData;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api("定价测算")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/calculation")
@@ -21,6 +24,7 @@ public class CalculationController {
     private final CalculationAppService calculationAppService;
 
     @PostMapping("/calculate")
+    @ApiOperation("测算")
     public List<CalculationNodeResult> calculate(@RequestBody CalculationCommand<AbstractCalculationData> command) {
         return calculationAppService.calculate(command);
     }
---
+++ common/src/main/java/configuration/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableOpenApi
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.OAS_30)
+                .enable(true)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
