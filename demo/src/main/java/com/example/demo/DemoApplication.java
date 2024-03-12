02024fa23110e4bd7409a939c150df2fba4a8d2c
lin <120377843@qq.com>
2024-03-12 23:10:07 +0800

    feat: make demo startable

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
+import enabler.EnableAuthService;
 import enabler.EnableBusinessRule;
 import com.example.calculation.infrastructure.util.EnableCalculation;
+import enabler.EnableDataDictionary;
 import enabler.EnableDistributeLock;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableBusinessRule
 @EnableCalculation
-@EnableDistributeLock
+@EnableAuthService
+@EnableDataDictionary
+@MapperScan(basePackages = {"com.example.demo.infrastructure", "com.example.calculation.infrastructure"})
 public class DemoApplication {
 
     public static void main(String[] args) {
