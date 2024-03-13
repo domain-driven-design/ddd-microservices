25d470d7c8dc7ef9c687e5c0d7cea84007df8d75
lin <120377843@qq.com>
2024-03-13 13:32:19 +0800

    feat: fix update issue

--- demo/src/main/java/com/example/demo/application/dto/request/UpdateTicketCommand.java
+++ demo/src/main/java/com/example/demo/application/dto/request/UpdateTicketCommand.java
@@ -2,11 +2,13 @@ package com.example.demo.application.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateTicketCommand {
     @NotNull
     private String title;
